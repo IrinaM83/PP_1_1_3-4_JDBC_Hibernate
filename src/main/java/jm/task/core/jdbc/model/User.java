af19b9f76c0155293e054c06baf3acd74ef39793
IrinaM83 <irochek-mikh89@mail.ru>
2022-08-22 21:45:07 +0300

    Hibernate

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,12 +1,12 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Table
+@Entity
+@Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
@@ -62,10 +62,10 @@ public class User {
 
     @Override
     public String toString() {
-        return '\n' + "Пользователь{" +
-                "ИД=" + id +
-                ", Имя= '" + name + '\'' +
-                ", Фамилия ='" + lastName + '\'' +
+        return '\n' + "????????????{" +
+                "??=" + id +
+                ", ???= '" + name + '\'' +
+                ", ??????? ='" + lastName + '\'' +
                 ", age=" + age +
                 '}';
     }
